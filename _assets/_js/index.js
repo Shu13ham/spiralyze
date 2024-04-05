b0422551ad796cc9ccfda528b5ce508ae949ba3c
SurviShirodkar22 <imshubham.here@gmail.com>
2024-04-06 00:51:42 +0530

    desktop

--- _assets/_js/index.js
+++ _assets/_js/index.js
@@ -1,19 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    startAutoPlay();
+document.addEventListener("DOMContentLoaded", function () {
+  startAutoPlay();
 });
 
-let currentIndex = 0;
-let autoPlayTimer;
+// JavaScript for slider functionality
+let currentSlide = 0;
+const slides = document.querySelectorAll(".testimonial.slide");
+const totalSlides = slides.length;
+const slideWidth = slides[0].clientWidth;
 
-function moveSlide(offset) {
-  const slides = document.querySelectorAll('.slide');
-  currentIndex = (currentIndex + offset + slides.length) % slides.length;
-  const displacement = -100 * currentIndex;
-  document.querySelector('.slides').style.transform = `translateX(${displacement}%)`;
+function moveSlide(n) {
+  const slideOffset = -n * slideWidth;
+  document.querySelector(
+    ".slides"
+  ).style.transform = `translateX(${slideOffset}px)`;
 }
 
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  moveSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  moveSlide(currentSlide);
+}
+
+document.querySelector(".prev").addEventListener("click", prevSlide);
+document.querySelector(".next").addEventListener("click", nextSlide);
+
 function startAutoPlay() {
-  autoPlayTimer = setInterval(function() {
+  autoPlayTimer = setInterval(function () {
     moveSlide(1);
   }, 5000); // Adjust autoplay interval (in milliseconds)
 }
@@ -37,12 +53,28 @@ function drag(event) {
 function endDrag() {
   const sensitivity = 50; // Adjust this value to control the sensitivity of the drag
   const dragDistance = startX - endX;
-  
+
   if (dragDistance > sensitivity) {
     moveSlide(1); // Move to next slide
   } else if (dragDistance < -sensitivity) {
     moveSlide(-1); // Move to previous slide
   }
-  
+
   startAutoPlay();
 }
+
+document.querySelectorAll('.info-card-router').forEach((card) => {
+  card.addEventListener('mouseover', (event) => {
+    event.currentTarget.querySelector('.on-hover-para').classList.add('d-block');
+    event.currentTarget.querySelector('.on-hover-para').classList.remove('d-none');
+    event.currentTarget.querySelector('.info-card-router .info-icon img').classList.add('d-block');
+    event.currentTarget.querySelector('.info-card-router .info-icon img').classList.remove('d-none');
+  });
+
+  card.addEventListener('mouseout', (event) => {
+    event.currentTarget.querySelector('.on-hover-para').classList.add('d-none');
+    event.currentTarget.querySelector('.on-hover-para').classList.remove('d-block');
+    event.currentTarget.querySelector('.info-card-router .info-icon img').classList.add('d-none');
+    event.currentTarget.querySelector('.info-card-router .info-icon img').classList.remove('d-block');
+  });
+});
